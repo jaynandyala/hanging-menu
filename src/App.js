994084ef3bc6a362412cd60961bc9765ed84ed84
JayNandyala <=>
2018-12-10 17:45:45 +0530

    mobiscroll fields

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import mobiscroll from '@mobiscroll/react';
+import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <mobiscroll.Page>
+                    <mobiscroll.Form>
+                        <mobiscroll.Input placeholder="text1"/>
+
+                        <mobiscroll.Input placeholder="text2"/>
+
+                        <mobiscroll.Input placeholder="text3"/>
+
+                        <mobiscroll.Input placeholder="text4"/>
+
+                        <mobiscroll.Input placeholder="text5"/>
+                    </mobiscroll.Form>
+                    <mobiscroll.BottomNav theme="ios" type="bottom" >
+                        <mobiscroll.NavItem icon="newspaper">News</mobiscroll.NavItem>
+                        <mobiscroll.NavItem icon="material-people" badge="1">People</mobiscroll.NavItem>
+                        <mobiscroll.NavItem icon="bubble" badge="9">Messages</mobiscroll.NavItem>
+                        <mobiscroll.NavItem icon="fa-globe">Notifications</mobiscroll.NavItem>
+                        <mobiscroll.NavItem icon="camera">Your story</mobiscroll.NavItem>
+                        <mobiscroll.NavItem icon="calendar">Events</mobiscroll.NavItem>
+                        <mobiscroll.NavItem icon="line-settings">Preferences</mobiscroll.NavItem>
+                    </mobiscroll.BottomNav>
+                </mobiscroll.Page>
+            </div>
+        );
+    }
 }
 
 export default App;
