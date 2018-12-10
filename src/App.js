607294c5d647648ea50e9f1a5836f947d3ae398e
JayNandyala <=>
2018-12-10 17:56:54 +0530

    mobiscroll select

--- src/App.js
+++ src/App.js
@@ -5,6 +5,41 @@ import mobiscroll from '@mobiscroll/react';
 import '@mobiscroll/react/dist/css/mobiscroll.min.css';
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            items: [{
+                value: 1,
+                text: 'Atlanta'
+            }, {
+                value: 2,
+                text: 'Berlin'
+            }, {
+                value: 3,
+                text: 'Boston'
+            }, {
+                value: 4,
+                text: 'Chicago'
+            }, {
+                value: 5,
+                text: 'London'
+            }, {
+                value: 6,
+                text: 'Los Angeles'
+            }, {
+                value: 7,
+                text: 'New York'
+            }, {
+                value: 8,
+                text: 'Paris'
+            }, {
+                value: 9,
+                text: 'San Francisco'
+            }],
+            val: 1
+        };
+    }
+
     render() {
         return (
             <div className="App">
@@ -19,8 +54,21 @@ class App extends Component {
                         <mobiscroll.Input placeholder="text4"/>
 
                         <mobiscroll.Input placeholder="text5"/>
+                        <mobiscroll.FormGroup>
+                            <mobiscroll.FormGroupTitle>Inside Mobiscroll Form</mobiscroll.FormGroupTitle>
+                            <label>
+                                Select
+                                <mobiscroll.Select
+                                    minWidth={200}
+                                    value={this.state.value}
+                                    data={this.state.items}
+                                    filter={true}
+                                >
+                                </mobiscroll.Select>
+                            </label>
+                        </mobiscroll.FormGroup>
                     </mobiscroll.Form>
-                    <mobiscroll.BottomNav theme="ios" type="bottom" >
+                    <mobiscroll.BottomNav theme="ios" type="bottom">
                         <mobiscroll.NavItem icon="newspaper">News</mobiscroll.NavItem>
                         <mobiscroll.NavItem icon="material-people" badge="1">People</mobiscroll.NavItem>
                         <mobiscroll.NavItem icon="bubble" badge="9">Messages</mobiscroll.NavItem>
